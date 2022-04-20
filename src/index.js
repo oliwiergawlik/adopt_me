d1cef2f95851145203a0c7f2d52ae04569aaa800
olivergav <oliwier.gawlik0@gmail.com>
2022-04-20 19:18:12 +0200

    props definitive explanation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h2", {}, "Luna"),
-        React.createElement("h3", {}, "Dog"),
-        React.createElement("h3", {}, "Havanese"),
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed),
     ]);
 }
 
@@ -11,9 +11,18 @@ const App = () => {
         "div",
         {},
         React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet),
+        React.createElement(Pet, {
+            //props:
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, {
+            //props:
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel"
+        }),
     );
 }
 
