20855cd3012981680bff9fb8a439c2a33eed555a
olivergav <oliwier.gawlik0@gmail.com>
2022-04-21 20:01:56 +0200

    Added fetch function and select for animal breed

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 ## Dictionary
 - CDN - Content Delivery Network
 - Transpilator - (i. e. Babel, Typescript) -> Transpilation == transformation and compilation. Conversion between languages versions.
-
+- SPA - Single Page Application
+- PWA - Progressive Page Application
+- SSA - Server side rendering
 
 ## Tools
 - Prettier - code formatter => [Link](https://www.npmjs.com/package/prettier) <=
 - Linter (ESLint- static code analyzer with guideline (Airbnb) => [Link](https://eslint.org/)  <=
 - Module Bundler (WebPack, Parcel)
-
+- StoryBook - 
+- State management library - (Flux, Redux, Mobx)
 
 ## Snippets:
 
--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pet from "./Pet";
 
 const animals = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 function SearchParams() {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const breeds = [""];
+
+  useEffect(() => {
+    requestPets().catch(() => {});
+  }, []);
+
+  async function requestPets() {
+    const response = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const data = await response.json();
+    setPets(data.pets);
+  }
 
   return (
     <div className={"search-params"}>
@@ -39,8 +56,32 @@ function SearchParams() {
           ))}
         </select>
 
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          disabled={!breeds.length}
+          value={breed}
+          onChange={(event) => {
+            setBreed(event.target.value);
+          }}
+          onBlur={(event) => {
+            setBreed(event.target.value);
+          }}
+        >
+          <option />
+          {breeds.map((breed) => (
+            <option value={breed} key={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
+
         <button type={"submit"}>Submit</button>
       </form>
+
+      {pets.map(({ name, breed, animal, id }) => (
+        <Pet key={id} name={name} breed={breed} animal={animal} />
+      ))}
     </div>
   );
 }
