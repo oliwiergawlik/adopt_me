a64fe30b7e56dd22dbe5aa28b5dbd4001d749b2e
olivergav <oliwier.gawlik0@gmail.com>
2022-07-05 23:08:42 +0200

    add test for useBreedList, mocking fetch to fetch API

--- src/__tests__/useBreedList.test.js
+++ src/__tests__/useBreedList.test.js
@@ -6,12 +6,13 @@ import {expect, test} from "@jest/globals";
 import {render} from "@testing-library/react";
 import useBreedList from "../hooks/useBreedList";
 import {renderHook} from "@testing-library/react";
+import {act} from "react-dom/test-utils";
 
-
-function getBreedList(animal){
+// Prepared function with component to isolate useBreedList hook inside component
+function getBreedList(animal) {
     let list;
 
-    function TestComponent(){
+    function TestComponent() {
         list = useBreedList(animal);
         return null;
     }
@@ -20,19 +21,54 @@ function getBreedList(animal){
     return list;
 }
 
-test('gives an empty list with no animals - new solution', async () => {
+test('gives an empty list with no animals', async () => {
     const [breedList, status] = getBreedList();
 
     expect(breedList).toHaveLength(0);
     expect(status).toBe('unloaded');
 })
 
-// ----------- New solution
+// ----------- New solution using renderHook
 
-test('gives an empty list with no animals', async () => {
+test('gives an empty list with no animals - new solution', async () => {
     const {result} = renderHook(() => useBreedList())
 
     const [breedList, status] = result.current;
     expect(breedList).toHaveLength(0);
     expect(status).toBe('unloaded');
+})
+
+// gives back breeds with an animal
+
+test('gives back breeds with an animal', async () => {
+    const breeds = [
+        "Havanese",
+        "Bichon Frise",
+        "Poodle",
+        "Maltese",
+        "Golden Retriever",
+        "Labrador",
+        "Husky",
+    ]
+
+    fetch.mockResponseOnce(
+        JSON.stringify({
+            animal: "dog",
+            breeds
+        })
+    )
+
+    //Ugly, but working
+    // let result_ = await act(() => {
+    //     const {result} = renderHook(() => useBreedList('dog'));
+    //     result_ = result;
+    // })
+    // const [breedList, status] = result_.current;
+
+    // Codewars example xd
+    let result = await act(() => renderHook(() => useBreedList('dog')).result)
+    const [breedList, status] = result.current;
+
+    expect(status).toBe('loaded');
+    expect(breedList).toEqual(breeds);
 })
