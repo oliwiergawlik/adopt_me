2e633890d8951620806d8c2912cec9751b1919ac
olivergav <oliwier.gawlik0@gmail.com>
2022-07-06 14:56:30 +0200

    basic test for Pet component, written again to practice

--- src/__tests__/Pet.test.js
+++ src/__tests__/Pet.test.js
@@ -7,19 +7,14 @@ import {render} from '@testing-library/react';
 import {StaticRouter} from "react-router-dom/server";
 import Pet from '../components/Pet';
 
-
-test("displays a default thumbnail", async () => {
+test('displays a default thumbnail', async () => {
     const pet = render(<StaticRouter><Pet/></StaticRouter>);
-
     const petThumbnail = await pet.findByTestId('thumbnail');
     expect(petThumbnail.src).toContain('none.jpg');
-});
+})
 
-
-test('displays a non-default thumbnail', async () => {
-    const pet = render(<StaticRouter><Pet images={['1.jpg', '2.png', '3.webm']}/></StaticRouter>);
-
+test('displays a non default thumbnail', async () => {
+    const pet = render(<StaticRouter><Pet images={['1.jpg', '2.jpg', '3.jpg', '4.jpg']}/></StaticRouter>);
     const petThumbnail = await pet.findByTestId('thumbnail');
     expect(petThumbnail.src).toContain('1.jpg')
-
-});
+})
